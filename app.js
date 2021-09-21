c56ad5b0d5bbb00ddbdab49503ac1f286fd17a92
hexachordal <francois.harewood@gmail.com>
2021-09-21 16:45:21 -0500

    commit on master

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -8,44 +10,107 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var LikeButton = function (_React$Component) {
-  _inherits(LikeButton, _React$Component);
+var App = function (_React$Component) {
+    _inherits(App, _React$Component);
 
-  function LikeButton(props) {
-    _classCallCheck(this, LikeButton);
+    function App(props) {
+        _classCallCheck(this, App);
 
-    var _this = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
+        var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-    _this.state = { liked: false };
-    return _this;
-  }
-
-  _createClass(LikeButton, [{
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
-
-      if (this.state.liked) {
-        fetch('https://restcountries.eu/rest/v2/all').then(function (response) {
-          return response.json();
-        }).then(function (data) {
-          return console.log(data);
-        });
-        return "something";
-      }
-
-      return React.createElement(
-        'button',
-        { onClick: function onClick() {
-            return _this2.setState({ liked: true });
-          } },
-        'Like'
-      );
+        _this.state = {
+            data: "",
+            flag: "",
+            name: "",
+            population: ""
+        };
+        return _this;
     }
-  }]);
 
-  return LikeButton;
+    _createClass(App, [{
+        key: "componentDidMount",
+        value: function componentDidMount() {
+            this.fetchData();
+        }
+    }, {
+        key: "fetchData",
+        value: function fetchData() {
+            var _this2 = this;
+
+            fetch("https://restcountries.eu/rest/v2/all").then(function (response) {
+                return response.json();
+            }).then(function (json) {
+                _this2.setState({
+                    data: json,
+                    flag: json[0].flag,
+                    name: json[0].name,
+                    population: json[0].population
+                });
+            }).catch(function (error) {
+                return console.log("parsed error", error);
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            var cArray = [];
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = Object.entries(this.state.data)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var _ref = _step.value;
+
+                    var _ref2 = _slicedToArray(_ref, 2);
+
+                    var key = _ref2[0];
+                    var value = _ref2[1];
+
+                    cArray.push(value);
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+
+            var countries = cArray.map(function (country) {
+                return React.createElement(
+                    "div",
+                    { id: "country" },
+                    React.createElement("img", { "class": "flag", src: country.flag }),
+                    "                      ",
+                    React.createElement(
+                        "h2",
+                        null,
+                        country.name
+                    ),
+                    React.createElement(
+                        "p",
+                        null,
+                        country.population
+                    )
+                );
+            });
+            return React.createElement(
+                "div",
+                { className: "box" },
+                countries
+            );
+        }
+    }]);
+
+    return App;
 }(React.Component);
 
-var domContainer = document.querySelector('#app');
-ReactDOM.render(React.createElement(LikeButton, null), domContainer);
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,26 +1,56 @@
 'use strict';
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-        fetch('https://restcountries.eu/rest/v2/all')
-        .then(response => response.json())
-        .then(data =>  console.log(data))
-        return "something"
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: "",
+            flag: "",
+            name: "",
+            population: ""
+        };
     }
 
-    return (
-      <button onClick={() => this.setState({ liked: true }) }>
-        Like
-      </button>
-    );
-  }
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch("https://restcountries.eu/rest/v2/all")
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    data: json,
+                    flag: json[0].flag,
+                    name: json[0].name,
+                    population: json[0].population
+                });
+            })
+            .catch(error => console.log("parsed error", error));
+    }
+
+
+
+    render() {
+        const cArray = []
+        for (const [key, value] of Object.entries(this.state.data)) {
+            cArray.push(value);
+        }
+
+
+        const countries = cArray.map((country) =>
+            <div id="country">
+                <img class="flag" src={country.flag} />                      <h2>{country.name}</h2>
+                <p>{country.population}</p>
+            </div>)
+        return <div className="box">
+            {countries}
+        </div>;
+    }
 }
 
-let domContainer = document.querySelector('#app');
-ReactDOM.render(<LikeButton />, domContainer);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
